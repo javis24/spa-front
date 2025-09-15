fc14ff22a53b483ecba41c32a4a8840d6d19abc4
WebGomezPalacio <luisjaviermtzb@gmail.com>
2025-09-15 12:40:59 -0600

    fix: config astro + tailwind + engines

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless'; 
 
-import 'dotenv/config';
-
-// https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+  output: 'server',
+  adapter: vercel(),
+    integrations: [], 
 });
